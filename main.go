f10dd6361b40c4b566acd0b5276f3c7adc32b44d
pspd0 <73247894+pspd0@users.noreply.github.com>
2023-07-27 17:10:33 +0900

    Update main.go

--- main.go
+++ main.go
@@ -5,230 +5,143 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"log"
-	"net/http"
 	"os"
 	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
-func Creater(w http.ResponseWriter, r *http.Request) {
-	if err := r.ParseForm(); err != nil {
-		fmt.Fprintf(w, "ParseForm() err: %v", err)
-		return
-	}
-	username := r.FormValue("username")
-	password := r.FormValue("password")
-
-	if username == "" || password == "" {
-		error := map[string]interface{}{
-			"result": false,
-			"text":   "Please enter your username or password",
-		}
-		jsonData, err := json.Marshal(error)
-		if err != nil {
-			fmt.Printf("Could not marshal json: %s\n", err)
-			return
-		}
-		fmt.Fprintf(w, "%s\n", jsonData)
-		return
-	}
-
-	if _, err := os.Stat("./account/" + username + ".json"); err == nil {
-		error := map[string]interface{}{
-			"result": false,
-			"text":   "Account already created",
-		}
-		jsonData, err := json.Marshal(error)
-		if err != nil {
-			fmt.Printf("Could not marshal json: %s\n", err)
-			return
-		}
-		fmt.Fprintf(w, "%s\n", jsonData)
-	} else {
-		h := sha256.New()
-		h.Write([]byte(username + password))
-		result := fmt.Sprintf("%x", h.Sum(nil))
-		data := map[string]interface{}{
-			"password": result,
-			"join":     time.Now(),
-		}
-		file, _ := json.MarshalIndent(data, "", " ")
-		os.WriteFile("./account/"+username+".json", file, 0644)
-		res := map[string]interface{}{
-			"result": true,
-			"text":   "Account creation complete",
-		}
-		jsonData, err := json.Marshal(res)
-		if err != nil {
-			fmt.Printf("Could not marshal json: %s\n", err)
-			return
-		}
-		fmt.Fprintf(w, "%s\n", jsonData)
-	}
-}
-
-func Login(w http.ResponseWriter, r *http.Request) {
-	if err := r.ParseForm(); err != nil {
-		fmt.Fprintf(w, "ParseForm() err: %v", err)
-		return
-	}
-	username := r.FormValue("username")
-	password := r.FormValue("password")
-
-	if username == "" || password == "" {
-		error := map[string]interface{}{
-			"result": false,
-			"text":   "Please enter your username or password",
-		}
-		jsonData, err := json.Marshal(error)
-		if err != nil {
-			fmt.Printf("Could not marshal json: %s\n", err)
-			return
-		}
-		fmt.Fprintf(w, "%s\n", jsonData)
-		return
-	}
-
-	if _, err := os.Stat("./account/" + username + ".json"); err != nil {
-		error := map[string]interface{}{
-			"result": false,
-			"text":   "This account does not exist",
-		}
-		jsonData, err := json.Marshal(error)
-		if err != nil {
-			fmt.Printf("Could not marshal json: %s\n", err)
-			return
-		}
-		fmt.Fprintf(w, "%s\n", jsonData)
-	} else {
-		h := sha256.New()
-		h.Write([]byte(username + password))
-		res := fmt.Sprintf("%x", h.Sum(nil))
-
-		jsonFile, err := os.Open("./account/" + username + ".json")
-		if err != nil {
-			fmt.Println(err)
-		}
-		defer jsonFile.Close()
-		byteValue, _ := ioutil.ReadAll(jsonFile)
-
-		var result map[string]interface{}
-		json.Unmarshal([]byte(byteValue), &result)
-		if res == result["password"] {
-			data := map[string]interface{}{
-				"result":   true,
-				"username": username,
-				"text":     "Login successful",
-			}
-			jsonData, err := json.Marshal(data)
-			if err != nil {
-				fmt.Printf("Could not marshal json: %s\n", err)
-				return
-			}
-			fmt.Fprintf(w, "%s\n", jsonData)
-		} else {
-			error := map[string]interface{}{
-				"result": false,
-				"text":   "The username or password is incorrect.",
-			}
-			jsonData, err := json.Marshal(error)
-			if err != nil {
-				fmt.Printf("Could not marshal json: %s\n", err)
-				return
-			}
-			fmt.Fprintf(w, "%s\n", jsonData)
-		}
-	}
-}
-
-func Delete(w http.ResponseWriter, r *http.Request) {
-	if err := r.ParseForm(); err != nil {
-		fmt.Fprintf(w, "ParseForm() err: %v", err)
-		return
-	}
-	username := r.FormValue("username")
-	password := r.FormValue("password")
-
-	if username == "" || password == "" {
-		error := map[string]interface{}{
-			"result": false,
-			"text":   "Please enter your username or password",
-		}
-		jsonData, err := json.Marshal(error)
-		if err != nil {
-			fmt.Printf("Could not marshal json: %s\n", err)
-			return
-		}
-		fmt.Fprintf(w, "%s\n", jsonData)
-		return
-	}
-
-	if _, err := os.Stat("./account/" + username + ".json"); err != nil {
-		error := map[string]interface{}{
-			"result": false,
-			"text":   "This account does not exist",
-		}
-		jsonData, err := json.Marshal(error)
-		if err != nil {
-			fmt.Printf("Could not marshal json: %s\n", err)
-			return
-		}
-		fmt.Fprintf(w, "%s\n", jsonData)
-	} else {
-		h := sha256.New()
-		h.Write([]byte(username + password))
-		res := fmt.Sprintf("%x", h.Sum(nil))
-
-		jsonFile, err := os.Open("./account/" + username + ".json")
-		if err != nil {
-			fmt.Println(err)
-		}
-		defer jsonFile.Close()
-		byteValue, _ := ioutil.ReadAll(jsonFile)
-
-		var result map[string]interface{}
-		json.Unmarshal([]byte(byteValue), &result)
-		if res == result["password"] {
-			if err != nil {
-				fmt.Printf("Error: %s\n", err)
-				return
-			}
-			data := map[string]interface{}{
-				"result":   true,
-				"username": username,
-				"text":     "Deleted successful",
-			}
-			jsonData, err := json.Marshal(data)
-			if err != nil {
-				fmt.Printf("Could not marshal json: %s\n", err)
-				return
-			}
-			fmt.Fprintf(w, "%s\n", jsonData)
-			go os.Remove("./account/" + username + ".json")
-			return
-		} else {
-			error := map[string]interface{}{
-				"result": false,
-				"text":   "The username or password is incorrect.",
-			}
-			jsonData, err := json.Marshal(error)
-			if err != nil {
-				fmt.Printf("Could not marshal json: %s\n", err)
-				return
-			}
-			fmt.Fprintf(w, "%s\n", jsonData)
-			return
-		}
-	}
-}
-
 func main() {
-	http.HandleFunc("/login", Login)
-	http.HandleFunc("/create", Creater)
-	http.HandleFunc("/delete", Delete)
+	server := gin.Default()
 
-	fmt.Println("localhost:8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
-	}
+	server.POST("/create", func(res *gin.Context) {
+		requestData, _ := ioutil.ReadAll(res.Request.Body)
+		var jsonBody map[string]interface{}
+		json.Unmarshal(requestData, &jsonBody)
+		username := jsonBody["username"].(string)
+		password := jsonBody["password"].(string)
+
+		if username == "" || password == "" {
+			res.JSON(404, gin.H{
+				"result": false,
+				"text":   "Please enter your username or password",
+			})
+		}
+
+		if _, err := os.Stat("./account/" + username + ".json"); err == nil {
+			res.JSON(404, gin.H{
+				"result": false,
+				"text":   "Account already created",
+			})
+		} else {
+			h := sha256.New()
+			h.Write([]byte(username + password))
+			result := fmt.Sprintf("%x", h.Sum(nil))
+			data := map[string]interface{}{
+				"password": result,
+				"join":     time.Now(),
+			}
+			file, _ := json.MarshalIndent(data, "", " ")
+			os.WriteFile("./account/"+username+".json", file, 0644)
+			res.JSON(200, gin.H{
+				"result": true,
+				"text":   "Account creation complete",
+			})
+		}
+	})
+
+	server.POST("/login", func(res *gin.Context) {
+		requestData, _ := ioutil.ReadAll(res.Request.Body)
+		var jsonBody map[string]interface{}
+		json.Unmarshal(requestData, &jsonBody)
+		username := jsonBody["username"].(string)
+		password := jsonBody["password"].(string)
+
+		if username == "" || password == "" {
+			res.JSON(200, gin.H{
+				"result": false,
+				"text":   "Please enter your username or password",
+			})
+		}
+
+		if _, err := os.Stat("./account/" + username + ".json"); err != nil {
+			res.JSON(200, gin.H{
+				"result": false,
+				"text":   "This account does not exist",
+			})
+		} else {
+			h := sha256.New()
+			h.Write([]byte(username + password))
+			result := fmt.Sprintf("%x", h.Sum(nil))
+			jsonFile, err := os.Open("./account/" + username + ".json")
+			if err != nil {
+				fmt.Println(err)
+			}
+			defer jsonFile.Close()
+			byteValue, _ := ioutil.ReadAll(jsonFile)
+			var r map[string]interface{}
+			json.Unmarshal([]byte(byteValue), &r)
+			if result == r["password"] {
+				res.JSON(200, gin.H{
+					"result":   true,
+					"username": username,
+					"text":     "Login successful",
+				})
+			} else {
+				res.JSON(200, gin.H{
+					"result": false,
+					"text":   "The username or password is incorrect.",
+				})
+			}
+		}
+	})
+
+	server.POST("/delete", func(res *gin.Context) {
+		requestData, _ := ioutil.ReadAll(res.Request.Body)
+		var jsonBody map[string]interface{}
+		json.Unmarshal(requestData, &jsonBody)
+		username := jsonBody["username"].(string)
+		password := jsonBody["password"].(string)
+
+		if username == "" || password == "" {
+			res.JSON(404, gin.H{
+				"result": false,
+				"text":   "Please enter your username or password",
+			})
+		}
+
+		if _, err := os.Stat("./account/" + username + ".json"); err != nil {
+			res.JSON(404, gin.H{
+				"result": false,
+				"text":   "This account does not exist",
+			})
+		} else {
+			h := sha256.New()
+			h.Write([]byte(username + password))
+			result := fmt.Sprintf("%x", h.Sum(nil))
+			jsonFile, err := os.Open("./account/" + username + ".json")
+			if err != nil {
+				fmt.Println(err)
+			}
+			defer jsonFile.Close()
+			byteValue, _ := ioutil.ReadAll(jsonFile)
+			var r map[string]interface{}
+			json.Unmarshal([]byte(byteValue), &r)
+			if result == r["password"] {
+				res.JSON(200, gin.H{
+					"result":   true,
+					"username": username,
+					"text":     "Deleted successful",
+				})
+				go os.Remove("./account/" + username + ".json")
+			} else {
+				res.JSON(404, gin.H{
+					"result": false,
+					"text":   "The username or password is incorrect.",
+				})
+			}
+		}
+	})
+
+	server.Run()
 }
